02d3df4367b93348ab6e469626f67567e9a7f01c
exsonebo <exkhfr@gmail.com>
2022-02-18 20:03:02 +0100

    Exercise Format done Other files generated during the training on learning PyCharm

--- ExerciseOne.py
+++ ExerciseOne.py
@@ -1,14 +1,14 @@
-#Ecrire un programme qui demande à l'utilisateur de saisir un nombre N.
+# Ecrire un programme qui demande à l'utilisateur de saisir un nombre N.
 
 nombre = input("Veuillez saisir un nombre: ")
 
-#Convertir nombre en integer
+# Convertir nombre en integer
 nombre = int(nombre) + 1
 
-#Le programme devra ensuite calculer puis afficher la somme des nombres compris entre 1 et N.
+# Le programme devra ensuite calculer puis afficher la somme des nombres compris entre 1 et N.
 SumNombre = 0
 
 for i in range(nombre):
     SumNombre = SumNombre + i
-    
+
 print(SumNombre)
